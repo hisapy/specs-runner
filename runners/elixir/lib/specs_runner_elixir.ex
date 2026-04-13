d772335c4c97f82750488e320caad0847068b2e4
Hisa Ishibashi <hisa@iporaitech.com>
2026-04-13 15:45:07 -0300

    Remove Elixir runner to reimplement it again

--- runners/elixir/lib/specs_runner_elixir.ex
+++
@@ -1,7 +0,0 @@
-defmodule SpecsRunnerElixir do
-  @moduledoc """
-  Entry point for the Elixir reference implementation of the specs runner.
-
-  The implementation will be built incrementally in later tasks.
-  """
-end
--- runners/elixir/test/specs_runner_elixir_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule SpecsRunnerElixirTest do
-  use ExUnit.Case
-
-  test "module loads" do
-    assert Code.ensure_loaded?(SpecsRunnerElixir)
-  end
-end
